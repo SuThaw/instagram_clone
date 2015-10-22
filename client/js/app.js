8c35c5954199e0ff8d09ac35ccae538b2c9dbe18
SuThaw <suthaw524@gmail.com>
2015-10-23 06:05:50 +0630

    Added route

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,21 @@
 angular.module('Instagram',['ngRoute','ngMessages'])
 	.config(function($routeProvider){
+		.when('/',{
+			templateUrl:'views/home.html',
+			controller:'HomeCtrl'
+		})
+		.when('/login',{
+			templateUrl:'views/login.html',
+			controller:'LoginCtrl'
 
+		})
+		.when('/signup',{
+			templateurl:'views/signup.html',
+			controller:'SignupCtrl'
+		})
+		.when('/photo/:id',{
+			templateurl:'views/detail.html',
+			controller:'DetailCtrl'
+		})
+		.otherwise('/')
 	});
