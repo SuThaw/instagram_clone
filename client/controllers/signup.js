06eb38b9d114a9d7828bace7bf0c632db8b8dea9
SuThaw <suthaw524@gmail.com>
2015-10-23 06:23:57 +0630

    Added home page views other views and controller

---
+++ client/controllers/signup.js
@@ -0,0 +1,4 @@
+angular.module('Instagram')
+	.controller('SignUpCtrl',function(){
+
+	});
--- client/js/app.js
+++ client/js/app.js
@@ -1,21 +1,22 @@
 angular.module('Instagram',['ngRoute','ngMessages'])
 	.config(function($routeProvider){
-		.when('/',{
-			templateUrl:'views/home.html',
-			controller:'HomeCtrl'
-		})
-		.when('/login',{
-			templateUrl:'views/login.html',
-			controller:'LoginCtrl'
+		$routeProvider
+			.when('/',{
+				templateUrl:'views/home.html',
+				controller:'HomeCtrl'
+			})
+			.when('/login',{
+				templateUrl:'views/login.html',
+				controller:'LoginCtrl'
 
-		})
-		.when('/signup',{
-			templateurl:'views/signup.html',
-			controller:'SignupCtrl'
-		})
-		.when('/photo/:id',{
-			templateurl:'views/detail.html',
-			controller:'DetailCtrl'
-		})
-		.otherwise('/')
+			})
+			.when('/signup',{
+				templateurl:'views/signup.html',
+				controller:'SignupCtrl'
+			})
+			.when('/photo/:id',{
+				templateurl:'views/detail.html',
+				controller:'DetailCtrl'
+			})
+			.otherwise('/')
 	});
